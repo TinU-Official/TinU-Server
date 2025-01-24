fe4c3b0f9eeeeaf0ba4d2f44990a66a16168534b
ReadMyMemory <lame3925@gmail.com>
2025-01-24 21:25:24 +0900

    [TINU-94] [Comment] : 간단한 주석 추가?

--- src/main/kotlin/com/tinuproject/tinu/domain/chat/config/ChatConfig.kt
+++ src/main/kotlin/com/tinuproject/tinu/domain/chat/config/ChatConfig.kt
@@ -17,7 +17,7 @@ class ChatConfig : WebSocketMessageBrokerConfigurer {
     }
 
     override fun configureMessageBroker(messageBrokerRegistry: MessageBrokerRegistry) {
-        messageBrokerRegistry.setApplicationDestinationPrefixes("/send"); // 도착 경로에 대한 prefix, /send/topic, /send/room 와 같이 된다.
-        messageBrokerRegistry.enableSimpleBroker("/topic", "/room"); // 메시지 브로커. topic은 해당 토픽을 구독하는 모두에게, room은 해당 룸을 구독한 사람 1명에게 보내는 경우 사용 예정.
+        messageBrokerRegistry.setApplicationDestinationPrefixes("/send"); // 도착 경로에 대한 prefix, /send/notice, /send/room 와 같이 된다.
+        messageBrokerRegistry.enableSimpleBroker("/notice", "/room"); // 메시지 브로커. notice은 공지와 같은 구독하는 모두에게, room은 해당 룸을 구독한 사람 1명에게 보내는 경우 사용 예정.
     }
 }
