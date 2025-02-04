49bcc1c2bff35ed1b646786ec6717f875eb45592
ReadMyMemory <lame3925@gmail.com>
2025-02-04 20:30:19 +0900

    [TINU-94] 코드 최신화 작업

--- src/main/kotlin/com/tinuproject/tinu/DTO/ResponseDTO.kt
+++
@@ -1,9 +0,0 @@
-package com.tinuproject.tinu.DTO
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class ResponseDTO(
-    @JsonProperty("success") val isSuccess: Boolean,
-    @JsonProperty("state") val stateCode: Int,
-    @JsonProperty("result") val result: Any?
-)
