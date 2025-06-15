3ad332209f6befab0550d3a2d071ceefd6be2f54
programofktw <thkim000317@naver.com>
2025-06-15 15:07:02 +0900

    [FIX] : client_secret 해석 EC 알고리즘으로 변경

--- src/main/kotlin/com/tinuproject/tinu/infra/security/oauth/apple/AppleJwtGenerator.kt
+++ src/main/kotlin/com/tinuproject/tinu/infra/security/oauth/apple/AppleJwtGenerator.kt
@@ -30,7 +30,7 @@ class AppleJwtGenerator(
             .setExpiration(from(exp))
             .setAudience("https://appleid.apple.com")
             .setSubject(appleProperties.clientId)
-            .signWith(loadPrivateKey(), SignatureAlgorithm.RS256)
+            .signWith(loadPrivateKey(), SignatureAlgorithm.ES256)
             .compact()
     }
 
@@ -48,7 +48,7 @@ class AppleJwtGenerator(
 
         val keyBytes = Base64.getDecoder().decode(privateKeyPem)
         val keySpec = PKCS8EncodedKeySpec(keyBytes)
-        return KeyFactory.getInstance("RSA").generatePrivate(keySpec)
+        return KeyFactory.getInstance("EC").generatePrivate(keySpec)
 
     }
 }
