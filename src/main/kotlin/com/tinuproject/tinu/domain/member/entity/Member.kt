669eae6fd8b2581f2d59e9c3dcd9161d843748e5
hynlee <hynlee@mobidoo.co.kr>
2026-04-25 15:42:38 +0900

    [issue177] : Member 엔티티 chatRoomMembers 양방향 매핑으로 정리

--- src/main/kotlin/com/tinuproject/tinu/domain/member/entity/Member.kt
+++ src/main/kotlin/com/tinuproject/tinu/domain/member/entity/Member.kt
@@ -4,7 +4,7 @@ import com.tinuproject.tinu.global.entity.BaseEntity
 import com.tinuproject.tinu.domain.member.enums.Social
 import com.tinuproject.tinu.domain.member.service.dto.input.UpdateUserInputDTO
 import com.tinuproject.tinu.domain.university.entity.University
-import com.tinuproject.tinu.domain.chat.entity.ChatRoom
+import com.tinuproject.tinu.domain.chat.entity.ChatRoomMember
 import com.tinuproject.tinu.domain.post.entity.Post
 import com.tinuproject.tinu.domain.post.entity.Scrap
 import jakarta.persistence.*
@@ -56,15 +56,16 @@ class Member (
         mappedBy = "member")
     var scrap : MutableList<Scrap> = mutableListOf(),
 
+    /**
+     * ChatRoom 이 buyer/seller 를 직접 참조하던 초기 설계는 608402d 에서
+     * ChatRoomMember 기반 모델로 정리되었다. Member ↔ ChatRoom 양방향은
+     * ChatRoomMember 컬렉션 하나로 통합한다 (이전의 buyerChatRoom/sellerChatRoom 제거).
+     * 더 큰 엔티티 모델링 재검토는 별도 회의에서 다룬다.
+     */
     @OneToMany(fetch = FetchType.LAZY,
         cascade = [CascadeType.REMOVE],
-        mappedBy = "buyer")
-    var buyerChatRoom: MutableList<ChatRoom> = mutableListOf(),
-
-    @OneToMany(fetch = FetchType.LAZY,
-        cascade = [CascadeType.REMOVE],
-        mappedBy = "seller")
-    var sellerChatRoom: MutableList<ChatRoom> = mutableListOf(),
+        mappedBy = "member")
+    var chatRoomMembers: MutableList<ChatRoomMember> = mutableListOf(),
 
     @OneToMany(fetch = FetchType.LAZY,
         cascade = [CascadeType.REMOVE],
